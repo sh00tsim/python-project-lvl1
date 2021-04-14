3de08f434e60bddc1cbd892166b2b744db5481f5
Redduck <393201@mail.ru>
2021-04-15 00:35:32 +0500

    Add scripts brain_even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import prompt
+from random import randint
+from brain_games.cli import welcome_user
+
+
+def is_even_number(number):
+    if number % 2 == 0:
+        return 'yes'
+    return 'no'
+
+def answer_user(number):
+    print('Question: {}'.format(number))
+    answer = prompt.string('Your answer: ')
+    return answer
+
+def even_number():
+    count_correct_answers = 0
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while count_correct_answers < 3:
+        number = randint(1, 99)
+        is_even = is_even_number(number)
+        user_answer = answer_user(number)
+        name_user = welcome_user.name
+        if is_even == user_answer:
+            print('Correct!')
+            count_correct_answers += 1
+        else:
+            return print("'{}' is wrong answer ;(. Correct answer was '{}'.\nLet's try again, {}!".format(user_answer, is_even, name_user))
+    return print('Congratulations, {}!'.format(name_user))
+
+def main():
+    welcome_user()
+    even_number()
+
+
+if __nmae__ == '__main__':
+    main()
